d7754639a22ebd50f44644170c0e75e421b6a8df
Рустам <rustam_gabdrakhmanov@yahoo.com>
2025-06-03 00:43:37 +0300

    Update script.js
    Добавил новые печенья, поменял валюту, исправил мелкие ошибки

--- public/js/script.js
+++ public/js/script.js
@@ -1,81 +1,82 @@
 const cookiesList = [
     {
-        name: '1',
-        desc: 'dasfasdf 1',
-        price: '1',
-        weght: '2 шт./ 200гр.',
-        img: './img/1.png'
+        name: '🟤 Пряничный дуэт',
+        desc: 'Классическое имбирное печенье с лёгкой хрустящей корочкой и тёплым ароматом специй. Идеально к чаю в холодный день.',
+        price: '100 ₽',
+        weght: '2 шт./ 200 г.',
+        img: './img/pngegg (4).png'
     },
     {
-        name: '2',
-        desc: 'dasfasdf 2',
-        price: '2',
-        weght: '2 шт./ 200гр.',
-        img: './img/2.png'
+        name: '🍬 Ореховая тарелка',
+        desc: 'Ассорти мини-печений с ореховой пастой и карамелью. Подходит для подарков и чаепитий.',
+        price: '680 ₽',
+        weght: '2 шт./ 420 г.',
+        img: './img/pngegg (3).png'
     },
     {
-        name: '3',
-        desc: 'dasfasdf 3',
-        price: '3',
-        weght: '2 шт./ 200гр.',
-        img: './img/3.png'
+        name: '🍥 Воздушные облака',
+        desc: 'Лёгкие безе-печенья с тающей текстурой. Белоснежные, нежные и идеально сладкие.',
+        price: '360 ₽',
+        weght: '6 шт./ 120 г.',
+        img: './img/pngegg (5).png'
     }
-    
 ];
 
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', () => {
     const productsList = document.querySelector('.products-list');
-    
-    cookiesList.forEach((cookie)=>{
+
+    cookiesList.forEach((cookie) => {
         const dpi = document.createElement('div');
-              dpi.setAttribute('class','product-item' );
+        dpi.setAttribute('class', 'product-item');
+
         const dib = document.createElement('div');
-              dib.setAttribute('class','product-img-box' );
+        dib.setAttribute('class', 'product-img-box');
+
         const pi = document.createElement('img');
-            pi.setAttribute('class','product-img' );
-            pi.setAttribute('alt', cookie.name );
-            pi.setAttribute('src',cookie.img );
+        pi.setAttribute('class', 'product-img');
+        pi.setAttribute('alt', cookie.name);
+        pi.setAttribute('src', cookie.img);
+
         const dpinfo = document.createElement('div');
-            dpinfo.setAttribute('class','product-info' );
+        dpinfo.setAttribute('class', 'product-info');
+
         const dpt = document.createElement('div');
-            dpt.setAttribute('class','product-title' );
-            dpt.innerText = cookie.name;
+        dpt.setAttribute('class', 'product-title');
+        dpt.innerText = cookie.name;
+
         const dpd = document.createElement('div');
-            dpd.setAttribute('class','product-description' );
-            dpd.innerText = cookie.desc;
+        dpd.setAttribute('class', 'product-description');
+        dpd.innerText = cookie.desc;
+
         const dpf = document.createElement('div');
-            dpf.setAttribute('class','product-footer' );
-        const dpun = document.createElement('div');
+        dpf.setAttribute('class', 'product-footer');
+
         const dpp = document.createElement('div');
-            dpp.setAttribute('class','product-price' );
-            dpp.setAttribute('data-current-price', '20');
-            dpp.innerText = cookie.price + ' $';
+        dpp.setAttribute('class', 'product-price');
+        dpp.innerText = cookie.price;
+
         const dpw = document.createElement('div');
-            dpw.setAttribute('class','product-weight' );
-            dpw.innerText = cookie.weght;
+        dpw.setAttribute('class', 'product-weight');
+        dpw.innerText = cookie.weght;
 
         const btn = document.createElement('button');
-            dpw.setAttribute('class','btn btn-primary' );
-            dpw.innerText = 'Заказать';
-        
+        btn.setAttribute('class', 'btn btn-primary');
+        btn.innerText = 'Заказать';
+
         dpi.append(dib);
         dib.append(pi);
         dpi.append(dpinfo);
         dpinfo.append(dpt);
         dpinfo.append(dpd);
         dpinfo.append(dpf);
-        dpf.append(dpun);
-        dpun.append(dpp);
-        dpun.append(dpw);
+        dpf.append(dpp);
+        dpf.append(dpw);
         dpf.append(btn);
 
         productsList.appendChild(dpi);
+    });
+});
 
-
-    })
-
-
-})
 
 
 
